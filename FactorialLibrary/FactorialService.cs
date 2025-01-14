91e2f275be353e725a7cebc17f848b1950441e46
gamebr8ker <42625897+gamebr8ker@users.noreply.github.com>
2025-01-14 17:53:45 +0000

    Create Solution

---
+++ FactorialLibrary/FactorialService.cs
@@ -0,0 +1,13 @@
+﻿namespace FactorialLibrary;
+
+public class FactorialService
+{
+    public static int Factorial( int n ) {
+        // Note: 0! = 1, 1! = 1
+        if (n == 1 || n == 0) {
+            return 1;
+        }
+
+        return n * Factorial(n-1);
+    }
+}
